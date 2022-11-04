ffcc06a50f91fac93e69db97f662480d64c63698
Ashwini <ashwinilakshmanan1@gmail.com>
2022-11-04 09:25:04 +0530

    header and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 function App() {
   return (
     <>
       <div >
+        <Header/>
         <ListEmployeeComponent/>
+        <Footer/>
       </div>
     </>
   );
